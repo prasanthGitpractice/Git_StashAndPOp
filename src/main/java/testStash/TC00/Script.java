fbc6803f570888f56460a58a0607983b266d56be
prasanthGitpractice <galiprasanth6@gmail.com>
2024-03-23 10:22:34 +0530

    master1

---
+++ src/main/java/testStash/TC00/Script.java
@@ -0,0 +1,7 @@
+package testStash.TC00;
+
+public class Script {
+	public static void main(String[] args) {
+		System.out.println("Pre-Req");
+	}
+}
---
+++ src/main/java/testStash/TC01/Script.java
@@ -0,0 +1,7 @@
+package testStash.TC01;
+
+public class Script {
+	public static void main(String[] args) {
+		System.out.println("Tc01");
+	}
+}
